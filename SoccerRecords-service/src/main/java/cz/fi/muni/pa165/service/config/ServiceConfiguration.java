459a9126922ee2116a21ca20c316c78e49988c78
Jaromir Sys <jarda.sys@gmail.com>
2016-11-29 18:21:56 +0100

    dozer mapping added

--- SoccerRecords-service/src/main/java/cz/fi/muni/pa165/service/config/ServiceConfiguration.java
+++ SoccerRecords-service/src/main/java/cz/fi/muni/pa165/service/config/ServiceConfiguration.java
@@ -9,21 +9,46 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import cz.fi.muni.pa165.PersistenceSampleApplicationContext;
+import cz.fi.muni.pa165.dto.GameDTO;
+import cz.fi.muni.pa165.dto.GoalDTO;
+import cz.fi.muni.pa165.dto.PlayerDTO;
 import cz.fi.muni.pa165.dto.TeamDTO;
+import cz.fi.muni.pa165.entity.Game;
+import cz.fi.muni.pa165.entity.Goal;
+import cz.fi.muni.pa165.entity.Player;
 import cz.fi.muni.pa165.entity.Team;
+import cz.fi.muni.pa165.service.GameServiceImpl;
+import cz.fi.muni.pa165.service.GoalServiceImpl;
+import cz.fi.muni.pa165.service.PlayerServiceImpl;
 import cz.fi.muni.pa165.service.TeamServiceImpl;
 
 @Configuration
 @Import(PersistenceSampleApplicationContext.class)
-@ComponentScan(basePackageClasses={TeamServiceImpl.class})
+@ComponentScan(basePackageClasses={TeamServiceImpl.class,GameServiceImpl.class,GoalServiceImpl.class,PlayerServiceImpl.class})
 public class ServiceConfiguration {
 	
 	@Bean
 	public Mapper dozer(){
 		DozerBeanMapper dozer = new DozerBeanMapper();		
-		//dozer.addMapping(new DozerCustomConfig());
+		dozer.addMapping(new DozerCustomConfig());
 		return dozer;
 	}
+
+    private static class DozerCustomConfig extends BeanMappingBuilder {
+
+        public DozerCustomConfig() {
+        }
+
+        @Override
+        protected void configure() {
+            mapping(Game.class, GameDTO.class);
+            mapping(Goal.class, GoalDTO.class);
+            mapping(Player.class,PlayerDTO.class);
+            mapping(Team.class,TeamDTO.class);
+        }
+
+
+    }
 		
 }
 
